7c144135f73727a685bc1da34df958617ba67a4c
K9921 <K9921@student.jamk.fi>
2017-01-25 20:03:21 +0200

    pientä säätöö

--- Teht10/Program.cs
+++ Teht10/Program.cs
@@ -10,8 +10,8 @@ namespace Teht10
     {
         static void Main(string[] args)
         {
-            int[] luvut = new int[9];
-            luvut[0] = 1;                   // alustetaan taulukkopaikkojen arvot
+            int[] luvut = new int[] { 1, 2, 33, 44, 55, 66, 77, 96, 100};
+            /*luvut[0] = 1;                   // alustetaan taulukkopaikkojen arvot
             luvut[1] = 2;
             luvut[2] = 33;
             luvut[3] = 44;
@@ -19,11 +19,12 @@ namespace Teht10
             luvut[5] = 66;
             luvut[6] = 77;
             luvut[7] = 96;
-            luvut[8] = 100;
+            luvut[8] = 100;*/
 
             for (int i = 0; i < luvut.Length; i++) // loopataan niin kauan kunnes taulukon kaikki paikat on käyty läpi
             {
                 if (i % 2 != 0) Console.WriteLine(luvut[i] + " HEP"); // jos luku on parillinen tulostetaan lisäksi "HEP"
+                 
                 else Console.WriteLine(luvut[i]);
             }
             Console.ReadLine();
--- Teht13/Program.cs
+++ Teht13/Program.cs
@@ -10,6 +10,22 @@ namespace Teht13
     {
         static void Main(string[] args)
         {
+            int i, j;
+            int[] pisteet = new int[5];   // tehdään taulukko
+            for (i = 0; i <= 4; i++)    // pyydetään ja luetaan viisi lukua taulukkoon
+            {
+                Console.Write("Anna hypyn pisteet: ");
+                int luku = int.Parse(Console.ReadLine());
+                pisteet[i] = luku;
+            }
+            for ()
+            {
+                if (pisteet[0] > pisteet[])             //vertailut
+                {
+                    Console.WriteLine("Suurin luku on: " + num1);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
